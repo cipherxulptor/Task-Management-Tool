4e47075a41b00df2f58149ff1a9abb7033d1e090
Prerana Kapadnis <prerana.kapadnis@autonomize.ai>
2026-02-02 06:42:30 +0530

    Added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/auth/Login'
+import SignUp from "./pages/auth/SignUp"
+import Dashboard from "./pages/admin/Dashboard"
+import ManageTasks from "./pages/admin/ManageTasks"
+import ManageUsers from "./pages/admin/ManageUsers"
+import CreateTask from "./pages/admin/CreateTask"
+import PrivateRoute from "./routes/PrivateRoute"
 
 const App = () => {
   return (
-    <div className="text-primary"> Task Manager App </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+
+
+          /* Admin Routes */
+          <Route element={<PrivateRoute allowedRoles={['admin']} />}></Route>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/tasks" element={<ManageTasks />} />
+            <Route path="/admin/users" element={<ManageUsers />} />
+            <Route path="/admin/create-task" element={<CreateTask />} />
+        </Routes>
+
+        /* User routes */
+        <Routes element ={<PrivateRoute allowedRoles={['user']} />}>
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="/user/tasks" element={<MyTasks />} />
+        <Route path="/user/task-details/:id" element={<TaskDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
