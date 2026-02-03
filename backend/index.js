9d73a43f0f2316ec75b75b97762fff13dd98efc4
Prerana Kapadnis <prerana.kapadnis@autonomize.ai>
2026-02-03 06:50:06 +0530

    sign-in user API

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.route.js"
 
@@ -26,6 +27,7 @@ app.use(cors({
 //Middleware to parse JSON request bodies//
 app.use(express.json())
 
+app.use(cookieParser())
 app.listen( 3000, () =>{
     console.log("Server is running on port 3000")
 })
