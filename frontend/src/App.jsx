e70a538b7699b7a540784b3201c04e5b1619984f
Prerana Kapadnis <prerana.kapadnis@autonomize.ai>
2026-02-04 20:29:04 +0530

    Login UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/auth/Login'
+import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/auth/Login"
 import SignUp from "./pages/auth/SignUp"
 import Dashboard from "./pages/admin/Dashboard"
 import ManageTasks from "./pages/admin/ManageTasks"
 import ManageUsers from "./pages/admin/ManageUsers"
 import CreateTask from "./pages/admin/CreateTask"
 import PrivateRoute from "./routes/PrivateRoute"
+import UserDashboard from "./pages/user/UserDashboard"
+import TaskDetails from "./pages/user/TaskDetails"
+import MyTasks from "./pages/user/MyTasks"
 
 const App = () => {
   return (
@@ -16,20 +19,20 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/sign-up" element={<SignUp />} />
 
-
-          /* Admin Routes */
-          <Route element={<PrivateRoute allowedRoles={['admin']} />}></Route>
+          {/* Admin Routes */}
+          <Route element={<PrivateRoute allowedRoles={["admin"]} />}>
             <Route path="/admin/dashboard" element={<Dashboard />} />
             <Route path="/admin/tasks" element={<ManageTasks />} />
             <Route path="/admin/users" element={<ManageUsers />} />
             <Route path="/admin/create-task" element={<CreateTask />} />
-        </Routes>
+          </Route>
 
-        /* User routes */
-        <Routes element ={<PrivateRoute allowedRoles={['user']} />}>
-        <Route path="/user/dashboard" element={<UserDashboard />} />
-        <Route path="/user/tasks" element={<MyTasks />} />
-        <Route path="/user/task-details/:id" element={<TaskDetails />} />
+          {/* User Routes */}
+          <Route element={<PrivateRoute allowedRoles={["user"]} />}>
+            <Route path="/user/dashboard" element={<UserDashboard />} />
+            <Route path="/user/tasks" element={<MyTasks />} />
+            <Route path="/user/task-details/:id" element={<TaskDetails />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
