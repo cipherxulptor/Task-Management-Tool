8815ebe6e75c11bf65176d89c9334a9bff83b64b
Prerana Kapadnis <prerana.kapadnis@autonomize.ai>
2026-02-05 14:45:27 +0530

    User card UI

--- frontend/src/pages/admin/ManageUsers.jsx
+++ frontend/src/pages/admin/ManageUsers.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axiosInstance from "../../utils/axioInstance"
 import DashboardLayout from "../../components/DashboardLayout"
+import { FaFileAlt } from "react-icons/fa"
 
 
 const ManageUsers = () => {
@@ -18,6 +19,8 @@ const ManageUsers = () => {
   }
   }
 
+  const handleDownloadReport = async () => {}
+
   useEffect(() => {
     getAllUsers()
 
@@ -25,7 +28,28 @@ const ManageUsers = () => {
   }, [])
 
   return (
-    <DashboardLayout activeMenu={"Team Members"}>ManageUsers</DashboardLayout>
+   <DashboardLayout activeMenu={"Team Members"}>
+      <div className="mt-5 mb-10">
+        <div className="flex items-center justify-between">
+          <h2 className="text-xl font-medium">Team Members</h2>
+
+          <button
+            type="button"
+            className="flex items-center gap-1 px-4 py-2 bg-yellow-100 hover:bg-yellow-200 text-gray-800 rounded-lg transition-colors duration-200 font-medium shadow-sm hover:shadow-md cursor-pointer text-lg"
+            onClick={handleDownloadReport}
+          >
+            <FaFileAlt className="text-lg" />
+            Download Report
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allUsers?.map((user) => (
+            <UserCard key={user._id} userInfo={user} />
+          ))}
+        </div>
+      </div>
+    </DashboardLayout>
   )
 }
 
